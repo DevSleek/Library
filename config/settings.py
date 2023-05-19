908b88933ff76fa037cf063adcffbb8001d8119c
SukhrobTuraev <suhrobturaev2004@gmail.com>
2023-05-19 09:07:19 +0500

    gunicorn

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -13,7 +14,7 @@ SECRET_KEY = 'django-insecure-ii+#qy3oa+@se%xk==vi#$@k#ayy&21ihu)#f*wict1lso9359
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # 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
 
@@ -127,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
